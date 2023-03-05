e68e75904daff3a064bb9e48eb07c1016242c374
kyzne <kyznetsovstepa@gmail.com>
2023-03-05 15:00:52 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -17,9 +17,11 @@ form.onsubmit = async (e) => {
       return
     }
 
-    input.classList.remove('form__input-nonvalid')
-    input.placeholder = 'название репозитория'
-
+    if (input.placeholder = 'минимальное число символов: 3' && input.classList.contains('form__input-nonvalid')) {
+      input.classList.remove('form__input-nonvalid')
+      input.placeholder = 'название репозитория'
+    }
+  
     let response = await fetch(`https://api.github.com/search/repositories?q=${input.value}&per_page=10`);
     let result = await response.json();
 
@@ -31,8 +33,9 @@ form.onsubmit = async (e) => {
         <img class = "repositories__avatar" src = ${element.owner.avatar_url} alt = "avatar" />
         <div class = "repositories__info">
           <a target = "_blank" class = "repositories__name" href = "${element.html_url}">${element.name}</a>
-          <span class = "repositories__author">Автор &#128578;: ${element.owner.login}</span>
-          <p class = "repositories__description">Описание &#128220;: ${element.description}</p>
+          <span class = "repositories__author">Автор &#128578;:  ${element.owner.login}</span>
+          <p class = "repositories__description">Описание &#128220;:  ${element.description || 'отсутствует'}</p>
+          <span class = "repositories__language">Язык программирования &#128187;:  ${element.language || 'отсутствует'}</span>
         </div>	
       </div>
       `
